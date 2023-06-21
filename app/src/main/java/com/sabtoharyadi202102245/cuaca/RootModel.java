6bb971dc14e738c8ec4356ae650900cf4183ebb6
SabtoHaryadi <harryjt858@gmail.com>
2023-06-21 10:50:57 +0700

    push

---
+++ app/src/main/java/com/sabtoharyadi202102245/cuaca/RootModel.java
@@ -0,0 +1,22 @@
+package com.sabtoharyadi202102245.cuaca;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class RootModel
+{
+    @SerializedName("list")
+    private List<ListModel> listModelList;
+
+    public List<ListModel> getListModelList() {
+        return listModelList;
+    }
+
+    public void setListModelList(List<ListModel> listModelList) {
+        this.listModelList = listModelList;
+    }
+
+    public RootModel() {
+    }
+}
